66223bbee2725bf9888873b14de05aceb166a6a4
thehansel07 <hanselcabreraespinal@gmail.com>
2024-10-02 20:37:16 -0400

    Agregando templates base

--- SistemaCompra/settings.py
+++ SistemaCompra/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SistemaCompraApp'
+    'SistemaCompraApp',
+    'sweetify'
 ]
 
 MIDDLEWARE = [
@@ -104,9 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santo_Domingo'
+
 
 USE_I18N = True
 
